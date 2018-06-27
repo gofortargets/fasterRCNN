fccb89030d801de775a404fcf0c340492a2e0035
Tuan Anh <anh_nguyen@knorex.com>
2018-06-27 15:17:45 +0800

    not val in trainval_net.py

--- test.py
+++ test.py
@@ -2,13 +2,4 @@ import torch
 
 # initilize the tensor holder here.
 im_data = torch.FloatTensor(1)
-im_info = torch.FloatTensor(1)
-num_boxes = torch.LongTensor(1)
-gt_boxes = torch.FloatTensor(1)
-
-# ship to cuda
-if True :
-    im_data = im_data.cuda()
-    im_info = im_info.cuda()
-    num_boxes = num_boxes.cuda()
-    gt_boxes = gt_boxes.cuda()
+im_data = im_data.cuda()
