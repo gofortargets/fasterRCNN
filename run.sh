90829e19c8902622ede4b2c5731f9d34ece5f134
Tuan Anh <anh_nguyen@knorex.com>
2018-07-22 09:49:16 +0800

    not val in trainval_net.py

--- run.sh
+++ run.sh
@@ -3,8 +3,12 @@
 #python trainval_net.py --_dataset pascal_voc --net vgg16 \
 #                       --save_dir models \
 #                       --cuda
-python trainval_net.py --dataset thilini --net vgg16 \
-                       --save_dir models \
-                       --cuda \
-                       --mGPUs \
-                       --bs 10
+#python trainval_net.py --dataset thilini --net vgg16 \
+#                       --save_dir models \
+#                       --cuda \
+#                       --mGPUs \
+#                       --bs 10
+
+python test_net.py --dataset thilini --net vgg16 \
+                   --checksession 1 --checkepoch 20 --checkpoint 39999 \
+                   --cuda
